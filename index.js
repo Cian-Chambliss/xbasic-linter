747ef9290c67aa312949990f168982850ed88f47
cian-chambliss <cian@alphasoftware.com>
2017-03-27 14:09:49 -0400

    Pass through options (staging for support for modes - like A5W/class def)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-var lintXbasic = function(code) {
+var lintXbasic = function(code,options) {
     var xbp = require("xbasic-parser");
-    var out = xbp.parse(code);
+    var out = xbp.parse(code,options);
     // TBD - add linting for non parse related errors (i.e. undimmed symbols etc)
     return out.error || [];
 };
